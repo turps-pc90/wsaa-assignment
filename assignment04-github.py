c5a6094f091acccd2f025ad0e3e4a4f1784342ad
turps-pc90 <callaghanpaul90@gmail.com>
2025-03-04 21:08:16 +0000

    Setting up assignment 4 file

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,1 +1,8 @@
 # Library Imports
+import requests
+import json
+from config import api_keys as keys
+
+# The aim of the script below is to access the andrews.txt file
+# from the GitHub repo and change the content of the file. 
+
