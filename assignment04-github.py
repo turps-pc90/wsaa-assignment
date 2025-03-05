0da37f1f14c667481880b56ac299e9352a80f53f
turps-pc90 <callaghanpaul90@gmail.com>
2025-03-05 20:31:52 +0000

    First successful connection to GitHub account using PyGithub library

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,8 +1,28 @@
 # Library Imports
 import requests
 import json
-from config import api_keys as keys
+from config import api_keys as keys, usernames as acc
+from github import Github, Auth
+
+# Using access token as per README - https://github.com/PyGithub/PyGithub
+auth = Auth.Token(keys["githubkey"])
+
+# Defining username
+ainm = acc["github_acc"]
 
 # The aim of the script below is to access the andrews.txt file
 # from the GitHub repo and change the content of the file. 
 
+g = Github(auth=auth)
+
+
+# Testing for successful connection
+user = g.get_user()
+print(f"Authenticated as: {user.login}")
+
+# Find GitHub objects
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    
+# Close connection
+g.close()
